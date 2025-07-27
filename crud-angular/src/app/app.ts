ab2974b823aaafc22a9aca61047c5dbdeb588f48
Andreson Souza <andresonjazz@gmail.com>
2025-07-27 01:19:23 -0300

    feat: criação do toolbar

--- crud-angular/src/app/app.ts
+++ crud-angular/src/app/app.ts
@@ -1,9 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
