a62feeb800d789d71396628cd94847246e57148f
Andreson Souza <andresonjazz@gmail.com>
2025-08-10 14:37:09 -0300

    chore: criação de componente e definição de interface

--- crud-angular/src/app/courses/courses/courses.ts
+++ crud-angular/src/app/courses/courses/courses.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.html',
   styleUrl: './courses.scss'
 })
-export class Courses {
+export class Courses implements OnInit {
+
+  courses: Course[] = [
+    { _id: '1', name: 'Angular Basics', category: 'Web Development' },
+    { _id: '2', name: 'Spring Boot Fundamentals', category: 'Backend Development' },
+    { _id: '3', name: 'Database Design', category: 'Database' }
+  ];
+
+  displayedColumns = ['name', 'category'];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    // Simulating fetching courses from a service
+  }
+
+
 
 }
